382ab241b9c2b7495d196c6c77dbd0803e8761f6
Hemant <guptahemant350@gmail.com>
2025-12-25 05:00:41 +0530

    Search bar: Inprogress

---
+++ components/SearchBar.tsx
@@ -0,0 +1,31 @@
+import { icons } from "@/constants/icons";
+import React from "react";
+import { Image, TextInput, View } from "react-native";
+
+interface Props {
+  onPress?: () => void;
+  placeholder: string;
+}
+
+const SearchBar = ({ onPress, placeholder }: Props) => {
+  return (
+    <View className="flex-row items-center bg-dark-200 rounded-full px-5 py-4 ">
+      <Image
+        source={icons.search}
+        className="size-5"
+        resizeMode="contain"
+        tintColor="#ab8bff"
+      />
+      <TextInput
+        onPress={onPress}
+        placeholder={placeholder}
+        placeholderTextColor="white"
+        className="ml-3 flex-1 text-white"
+        value=""
+        onChangeText={() => {}}
+      />
+    </View>
+  );
+};
+
+export default SearchBar;
