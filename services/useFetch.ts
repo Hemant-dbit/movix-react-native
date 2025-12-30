d01c4f86b43c8a34faeae43b306532a55922cfe1
Hemant <guptahemant350@gmail.com>
2025-12-30 22:01:37 +0530

    API: fetchMovies, CustomHook

---
+++ services/useFetch.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+
+const useFetch = <T>(fetchfunction: () => Promise<T>, autofetch = true) => {
+  const [data, setData] = useState<T | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<Error | null>(null);
+
+  const fetchData = async () => {
+    try {
+      setData(null);
+      setLoading(true);
+
+      const result = await fetchfunction();
+
+      setData(result);
+    } catch (err) {
+      setError(
+        err instanceof Error ? err : new Error("An unknown error occurred")
+      );
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const reset = () => {
+    setData(null);
+    setError(null);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (autofetch) {
+      fetchData();
+    }
+  }, []);
+
+  return { data, loading, error, refetch: fetchData, reset };
+};
+
+
+export default useFetch;
