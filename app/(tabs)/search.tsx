dcfcb8d5ddc611825c5fb788673af455c20ae44e
Hemant <guptahemant350@gmail.com>
2026-01-06 23:53:13 +0530

    Search Page: Connected to db

--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -3,6 +3,7 @@ import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
 import { fetchMovies } from "@/services/api";
+import { updateSearchCount } from "@/services/appwrite";
 import useFetch from "@/services/useFetch";
 import React, { useEffect, useState } from "react";
 import { ActivityIndicator, FlatList, Image, Text, View } from "react-native";
@@ -19,9 +20,14 @@ const search = () => {
   } = useFetch(() => fetchMovies({ query: searchQuery }), false);
 
   useEffect(() => {
+
     const timeoutId = setTimeout(async () => {
       if (searchQuery.trim()) {
         await refetchMovies();
+
+        if (movies?.length > 0 && movies[0]) {
+          await updateSearchCount(searchQuery, movies[0]);
+        }
       } else {
         reset();
       }
@@ -30,6 +36,8 @@ const search = () => {
     return () => clearTimeout(timeoutId);
   }, [searchQuery]);
 
+  
+  
   return (
     <View className="flex-1 bg-primary">
       <Image
@@ -103,6 +111,6 @@ const search = () => {
       />
     </View>
   );
-};
+};;
 
 export default search;
